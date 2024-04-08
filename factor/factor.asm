ff996ffc09819924a68059508eed23adb239c6c4
Kaosifer <ockap.pnbepa2.0@gmail.com>
2024-04-09 01:44:18 +0200

    Modificaciones para Factor

--- factor/factor.asm
+++ factor/factor.asm
@@ -1,19 +1,22 @@
 _TEXT SEGMENT BYTE PUBLIC 'CODE'
   ASSUME CS:_TEXT
 PUBLIC _factor
-_factor PROC NEAR
+_factor PROC FAR
   push bp
   mov bp, sp
-
-  mov AX, 6[BP]
-  cmp AX, 1
-  je  .FIN
-  mov BX, AX
+  mov bx, 6[bp]
+  mov cx, 1
+  cmp bx, cx
+  je .FIN
+  dec bx
+  push bx 
   call _factor
-  mul BX
-.FIN:
-  pop bp
+  add sp, 2
+  mul WORD PTR 6[bp]
   ret
+.FIN:
+  mov ax, 1
+  sub sp, 6
 _factor ENDP
 _TEXT ENDS
 END
